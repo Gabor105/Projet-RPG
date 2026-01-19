6a8700e3724b15a3a45e8e58ea1ca1c50dc7e586
Emerick <pacaudemerick@gmail.com>
2026-01-19 09:05:31 +0100

    [Correction] correction de la classe objet

--- classes/Objets/Objet.ts
+++ classes/Objets/Objet.ts
@@ -1,9 +1,6 @@
 import { Character } from "../Character.ts";
 
 export class Objet{
-    private pourcentageVieSiEnVie : number = 0;
-    private pourcentageVieSiPasEnVie : number = 0;
-
     UtiliserObjet(character:Character){
         console.log("Cette fonction ne devrais pas être appeller !");
     }
