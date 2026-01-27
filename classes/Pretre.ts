a8dc208e58370a2424603401447a53df8faaf869
Emerick <pacaudemerick@gmail.com>
2026-01-27 07:38:01 +0100

    [Correction] correction de la classe Pretre pour évité qu'elle ne créé une erreur au démarage du programme

--- classes/Pretre.ts
+++ classes/Pretre.ts
@@ -1,8 +1,12 @@
 import { Aventurier } from "./Aventurier.ts";
+import { Character } from "./Character.ts";
 
 export class Pretre extends Aventurier{
     constructor(nom: string) {
         // nom, pvMax, attaque, defense, vitesse, pmMax, pmActuels
         super(nom, 110, 4, 6, 8);
+    }
+    public override jouerTour(ennemis: Character[], allies: Character[]): void {
+        // this.voler();
     }
 }
